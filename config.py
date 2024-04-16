e39360544f961618febe0ff1c4d2d282499ead8d
Jujin <jujin@jujinkim.com>
2024-04-17 03:18:33 +0900

    Implement main.py
    - renamed git_commiter to git_committer.py
    - added repo_path, repo_name, github_token at config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 url_tistory = ""
 title_tag_selector = "article#content .inner h1"
 article_tag_selector = "#article-view .tt_article_useless_p_margin.contents_style"
+repo_path = ""
+repo_name = ""
+github_token = ""
 
 def read_config():
     with open('truth_config.txt') as f:
@@ -12,10 +15,19 @@ def read_config():
                 globals()[name] = value
             elif name == 'article_tag_selector':
                 globals()[name] = value
+            elif name == 'repo_path':
+                globals()[name] = value
+            elif name == 'repo_name':
+                globals()[name] = value
+            elif name == 'github_token':
+                globals()[name] = value
 
     # write the config variables to truth_config.txt again
     with open('truth_config.txt', 'w') as f:
         f.write(f'url_tistory={url_tistory}\n')
         f.write(f'title_tag_selector={title_tag_selector}\n')
         f.write(f'article_tag_selector={article_tag_selector}\n')
+        f.write(f'repo_path={repo_path}\n')
+        f.write(f'repo_name={repo_name}\n')
+        f.write(f'github_token={github_token}\n')
     
--- main.py
+++ main.py
@@ -1,12 +1,53 @@
-import sys
 from config import *
+import argparse
+from git_committer import *
+from rss_downloader import *
+from article_downloader import *
+from hugo_post_creator import *
 
-def main(postNum):
+def main():
     read_config()
-    postUrl = url_tistory + "/" + postNum
-    print("Tistory post url: ", postUrl)
 
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--rss", help="if existed, download the article from the rss url")
+    parser.add_argument("--postNum", type=int, help="the post number of the tistory post")
+    parser.add_argument("--debug", help="instead of create commit, create debug file output")
+
+    args = parser.parse_args()
+    print("Tistory post url: ", url_tistory + args.postNum)
+
+    # 1. Create and checkout git branch
+    git = GitCommitter(repo_path, github_token, repo_name)
+    branch_name = git.checkout_n_create_branch(args.postNum)
+
+
+    # 2. Download article
+    if (args.rss):
+        rssdl = RssDownloader()
+        article = rssdl.get_feed_entries(args.rss, args.postNum)
+    else:
+        articledl = ArticleDownloader()
+        article = articledl.download_article(url_tistory + args.postNum)
+
+    print("Title: ", article.title)
+    print("Url: ", article.url)
+    if (args.debug):
+        print("Article: ", article.content)
+        with open('post_debug.md', 'w') as f:
+            f.write(f'# {article.title}\n\n')
+            f.write(f'[Link]({article.url})\n\n')
+            f.write(article.content)
+        return
+
+    # 3. Create post file
+    post_creator = HugoPostCreator()
+    post_creator.create_post(article, f'{repo_path}/content/posts/{args.postNum}.md')
+
+    # 4. Push to the git repository
+    git.commit_and_push_to_postnum_branch(args.postNum)
+
+    # 5. Create pull request
+    git.create_pull_request_n_cleanup(args.postNum)
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main()
